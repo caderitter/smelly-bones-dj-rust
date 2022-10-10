6e766e946d50ecce468c2aeded50a69044ebe76d
caderitter <ritter.cade@gmail.com>
2022-10-10 02:19:25 -0500

    queue, skip

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use std::env;
-
-use serenity::client::Context;
+use std::{env, sync::Arc};
 
 use serenity::{
     async_trait,
+    client::Context,
     client::{Client, EventHandler},
     framework::{
         standard::{
             macros::{command, group},
-            Args, CommandResult,
+            Args, Command, CommandResult,
         },
         StandardFramework,
     },
     model::{channel::Message, gateway::Ready},
-    prelude::GatewayIntents,
+    prelude::{GatewayIntents, Mutex},
     Result as SerenityResult,
 };
-use songbird::SerenityInit;
+
+use songbird::{input::Restartable, SerenityInit, Call, Songbird};
+use tokio::sync::MutexGuard;
 
 struct Handler;
 
@@ -28,7 +29,7 @@ impl EventHandler for Handler {
 }
 
 #[group]
-#[commands(play)]
+#[commands(play, queue, skip)]
 struct General;
 
 #[tokio::main]
@@ -121,6 +122,161 @@ async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
     Ok(())
 }
+
+#[command]
+#[only_in(guilds)]
+async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let url = match args.single::<String>() {
+        Ok(url) => url,
+        Err(_) => {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, "Must provide a URL to a video or audio")
+                    .await,
+            );
+
+            return Ok(());
+        }
+    };
+
+    if !url.starts_with("http") {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Must provide a valid URL")
+                .await,
+        );
+
+        return Ok(());
+    }
+
+    let guild = msg.guild(&ctx.cache).unwrap();
+    let guild_id = guild.id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let handler_lock = match manager.get(guild_id) {
+        Some(handler_lock) => handler_lock,
+        None => {
+            let channel_id = guild
+                .voice_states
+                .get(&msg.author.id)
+                .and_then(|voice_state| voice_state.channel_id);
+
+            let channel = match channel_id {
+                Some(channel) => channel,
+                None => {
+                    check_msg(
+                        msg.channel_id
+                            .say(&ctx.http, "Not in a voice channel")
+                            .await,
+                    );
+                    return Ok(());
+                }
+            };
+
+            let (handler_lock, _) = manager.join(guild_id, channel).await;
+            handler_lock
+        }
+    };
+
+    let mut handler = handler_lock.lock().await;
+
+    let source = match Restartable::ytdl(url, true).await {
+        Ok(source) => source,
+        Err(why) => {
+            println!("Err starting source: {:?}", why);
+
+            check_msg(msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await);
+
+            return Ok(());
+        }
+    };
+
+    handler.enqueue_source(source.into());
+
+    check_msg(
+        msg.channel_id
+            .say(
+                &ctx.http,
+                format!("Added song to queue: position {}", handler.queue().len()),
+            )
+            .await,
+    );
+
+    Ok(())
+}
+
+#[command]
+#[only_in(guilds)]
+async fn skip(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).unwrap();
+    let guild_id = guild.id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let handler_lock = match manager.get(guild_id) {
+        Some(handler_lock) => handler_lock,
+        None => {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, "Not in a voice channel")
+                    .await,
+            );
+            return Ok(());
+        }
+    };
+
+    let handler = handler_lock.lock().await;
+    let queue = handler.queue();
+    match queue.skip() {
+        Ok(_) => (),
+        Err(why) => {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("There was an error skipping the queue: ${:?}", why))
+                    .await,
+            );
+            return Ok(());
+        }
+    };
+    check_msg(msg.channel_id.say(&ctx.http, "Skipped").await);
+    Ok(()) 
+}
+
+// #[command]
+// #[only_in(guilds)]
+// async fn playfirst(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+//     let guild = msg.guild(&ctx.cache).unwrap();
+//     let guild_id = guild.id;
+
+//     let manager = songbird::get(ctx)
+//         .await
+//         .expect("Songbird Voice client placed in at initialisation.")
+//         .clone();
+
+//     let handler_lock = match manager.get(guild_id) {
+//         Some(handler_lock) => handler_lock,
+//         None => {
+//             check_msg(
+//                 msg.channel_id
+//                     .say(&ctx.http, "Not in a voice channel")
+//                     .await,
+//             );
+//             return Ok(());
+//         }
+//     };
+
+//     let handler = handler_lock.lock().await;
+//     let queue = handler.queue();
+//     queue.modify_queue(|q| q.push_front(value));
+
+// }
 
 fn check_msg(result: SerenityResult<Message>) {
     if let Err(why) = result {
